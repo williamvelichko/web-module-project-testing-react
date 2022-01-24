ef73a9316bbae53eafcb69367d49873415a16f26
williamvelichko <williamvelichko2003@gmail.com>
2022-01-24 13:02:19 -0800

    testing episode.js

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -1,11 +1,31 @@
-import React from 'react';
-import '@testing-library/jest-dom/extend-expect';
-import { render, screen } from '@testing-library/react';
-import Episode from './../Episode';
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import { render, screen } from "@testing-library/react";
+import Episode from "./../Episode";
+import { unstable_renderSubtreeIntoContainer } from "react-dom";
 
+//import { fetchShow } from "./../../api/fetchShow";
+//jest.mock("./../../api/fetchShow");
 
-test("renders without error", () => {});
+const fakeEpisode = [
+  {
+    id: 1,
+    image:
+      "https://static.tvmaze.com/uploads/images/medium_landscape/342/855788.jpg",
+    name: "name",
+    season: 3,
+    number: 3,
+    summary: "runaway",
+    runtime: 30,
+  },
+];
 
-test("renders the summary test passed as prop", ()=>{});
+test("renders without error", () => {
+  render(<Episode episode={fakeEpisode} />);
+});
 
-test("renders default image when image is not defined", ()=>{});
+test("renders the summary test passed as prop", () => {
+  render(<Episode episode={fakeEpisode} />);
+});
+
+test("renders default image when image is not defined", () => {});
