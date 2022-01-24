59831faf0b8c3802a7dfda9f5316453ee1df4265
williamvelichko <williamvelichko2003@gmail.com>
2022-01-24 14:02:53 -0800

    finished the testing of episode.js

--- src/components/Episode.js
+++ src/components/Episode.js
@@ -1,22 +1,26 @@
-import React from 'react';
+import React from "react";
 
-const Episode = (props)=> {
-    const {episode} = props;
-    const {id, image, name, season, number, summary, runtime} = episode;
-    const imgsrc = image || 'https://i.ibb.co/2FsfXqM/stranger-things.png';
+const Episode = (props) => {
+  const { episode } = props;
+  const { id, image, name, season, number, summary, runtime } = episode;
+  const imgsrc = image || "./stranger_things.png";
 
-    console.log("episode: ", episode);
+  console.log("episode: ", episode);
 
-    return(<div className="episode" key={id}>
-        <img className="episode-image" src={imgsrc} alt={imgsrc} />
-        <div className="episode-info">
-            <p className="episode-number">Season {season}, Episode {number}</p>
-            <h3>{name}</h3>
-            <p>{summary}</p>
-            <div className="flex-spacer" />
-            <p className="episode-runtime">{runtime} minutes</p>
-        </div>
-    </div>)
-}
+  return (
+    <div className="episode" key={id}>
+      <img className="episode-image" src={imgsrc} alt={imgsrc} />
+      <div className="episode-info">
+        <p className="episode-number">
+          Season {season}, Episode {number}
+        </p>
+        <h3>{name}</h3>
+        <p>{summary}</p>
+        <div className="flex-spacer" />
+        <p className="episode-runtime">{runtime} minutes</p>
+      </div>
+    </div>
+  );
+};
 
 export default Episode;
--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -7,18 +7,26 @@ import { unstable_renderSubtreeIntoContainer } from "react-dom";
 //import { fetchShow } from "./../../api/fetchShow";
 //jest.mock("./../../api/fetchShow");
 
-const fakeEpisode = [
-  {
-    id: 1,
-    image:
-      "https://static.tvmaze.com/uploads/images/medium_landscape/342/855788.jpg",
-    name: "name",
-    season: 3,
-    number: 3,
-    summary: "runaway",
-    runtime: 30,
-  },
-];
+const fakeEpisode = {
+  id: 1,
+  image:
+    "https://static.tvmaze.com/uploads/images/medium_landscape/342/855788.jpg",
+  name: "name",
+  season: 3,
+  number: 3,
+  summary: "runaway",
+  runtime: 30,
+};
+
+const fakeEpisode2 = {
+  id: 1,
+  image: null,
+  name: "name",
+  season: 3,
+  number: 3,
+  summary: "runaway",
+  runtime: 30,
+};
 
 test("renders without error", () => {
   render(<Episode episode={fakeEpisode} />);
@@ -26,6 +34,23 @@ test("renders without error", () => {
 
 test("renders the summary test passed as prop", () => {
   render(<Episode episode={fakeEpisode} />);
+  //arrange: render
+
+  //act: find our summary
+  const summary = screen.queryByText(/runaway/i);
+  //console.log(summary);
+
+  //assert: confirm that it exists
+  expect(summary).toBeDefined();
+  expect(summary).toBeInTheDocument();
+  expect(summary).toBeTruthy();
 });
 
-test("renders default image when image is not defined", () => {});
+test("renders default image when image is not defined", () => {
+  render(<Episode episode={fakeEpisode2} />);
+
+  const imageUrl = screen.getByAltText("./stranger_things.png");
+  //console.log(imageUrl);
+
+  expect(imageUrl).toBeInTheDocument();
+});
